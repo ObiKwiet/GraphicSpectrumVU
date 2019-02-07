4be1e1478951b5259e81c5bb1ead928ab390ffd8
ObiKwiet <seth.c.tucker@gmail.com>
2019-02-07 10:31:34 -0500

    Warnings Fixed

--- VUMeter/Debug/objects.mk
+++ VUMeter/Debug/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS := C:/Downloads/Projects/Project1/VUMeter/libksdk_platform.a
+USER_OBJS := C:/Downloads/Projects/GraphicSpectrumVU/VUMeter/libksdk_platform.a
 
 LIBS := -larm_cortexM4lf_math
 
